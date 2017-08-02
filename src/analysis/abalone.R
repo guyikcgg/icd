983b076ee20d8e6cd12e6249a2704caadd6a352c
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-08-02 20:55:02 +0200

    Update workspace

---
+++ src/analysis/abalone.R
@@ -0,0 +1,119 @@
+################################################
+#      Introducción a la Ciencia de Datos      #
+#              ANÁLISIS DE DATOS               #
+#                                              #
+# (C) Cristian González Guerrero               #
+################################################
+
+# Load required libraries
+library(utils)
+library(stats)
+library(foreign)
+library(ggplot2)
+library(reshape)
+
+# ABALONE
+
+# Load the dataset and provide it with the 
+# structure from Keel
+abalone = read.csv(
+  "datasets/abalone/abalone.dat", 
+  comment.char="@"
+)
+names(abalone) = c(
+  "Sex", 
+  "Length", 
+  "Diameter", 
+  "Height", 
+  "Whole_weight", 
+  "Shucked_weight",
+  "Viscera_weight",
+  "Shell_weight",
+  "Rings"
+)
+
+
+abalone$Sex = factor(
+  abalone$Sex, 
+  levels = c(1, 2, 3), 
+  labels = c("M", "F", "I")
+  )
+
+
+# Get the data type and dimension
+class(abalone)
+dim(abalone)
+
+# Get the data types of every field
+lapply(abalone, class)
+
+# Check if there are missing values
+colSums(is.na(abalone))
+
+# Get the statistical measurements (numeric variables)
+abalone2 = subset(abalone, select = -Sex)
+
+sapply(abalone2, min)             # Minimum
+sapply(abalone2, quantile, 0.25)  # 1st Quartile
+sapply(abalone2, median)          # Median
+sapply(abalone2, mean)            # Mean
+sapply(abalone2, quantile, 0.75)  # 3rd Quartile
+sapply(abalone2, max)             # Max
+
+sapply(abalone2, var)             # Variane
+sapply(abalone2, sd)              # Standard deviation
+
+# Get summarized information
+str(abalone)      # Structure
+summary(abalone)  # Statistical measurements
+
+# Get correlation matrix
+cor(abalone2)
+
+# By taking a look to the file, it is clear that 
+# the output variable is Class, thus we have to 
+# classify the data by this category
+
+ggplot(abalone, aes(x = Sex)) + geom_bar(stat = "count")
+
+meltData = melt.data.frame(data = abalone[,2:8])
+ggplot(meltData, aes(factor(variable), value)) + geom_boxplot() + coord_flip()
+
+ggplot(abalone, aes(Sex, Rings)) + geom_boxplot()
+
+ggplot(abalone, aes(Rings)) + geom_histogram(bins = 29)
+
+ggplot(abalone, aes(Shell_weight)) + geom_histogram(bins = 29)
+
+ggplot(abalone, aes(Shell_weight,   colour = Sex)) + geom_freqpoly(bins = 29)
+ggplot(abalone, aes(Viscera_weight, colour = Sex)) + geom_freqpoly(bins = 29)
+ggplot(abalone, aes(Shucked_weight, colour = Sex)) + geom_freqpoly(bins = 29)
+ggplot(abalone, aes(Whole_weight,   colour = Sex)) + geom_freqpoly(bins = 29)
+ggplot(abalone, aes(Height,         colour = Sex)) + geom_freqpoly(bins = 29)
+ggplot(abalone, aes(Diameter,       colour = Sex)) + geom_freqpoly(bins = 29)
+ggplot(abalone, aes(Length,         colour = Sex)) + geom_freqpoly(bins = 29)
+ggplot(abalone, aes(Rings,          colour = Sex)) + geom_freqpoly(bins = 29)
+
+
+abalone.reshaped = melt(abalone[abalone$Height<0.3,]) # Remove outlayers
+ggplot(abalone.reshaped, aes(value, colour = Sex)) + geom_freqpoly(bins = 29) + facet_wrap( ~ variable, ncol = 2, scales = "free")
+# Histograms show that Male-Female distinction is not relevant, and does not characterize any of the above variables.
+# Nonetheless, Infants' dimensions are usually lower, as it is their age.
+# Statistical test is needed to confirm this statement.
+
+
+ggplot(abalone, aes(x = Shell_weight,   y = Rings)) + geom_jitter(alpha = 0.2)
+ggplot(abalone, aes(x = Viscera_weight, y = Rings)) + geom_jitter(alpha = 0.2)
+ggplot(abalone, aes(x = Shucked_weight, y = Rings)) + geom_jitter(alpha = 0.2)
+ggplot(abalone, aes(x = Whole_weight,   y = Rings)) + geom_jitter(alpha = 0.2)
+ggplot(abalone, aes(x = Diameter,       y = Rings)) + geom_jitter(alpha = 0.2)
+ggplot(abalone, aes(x = Length,         y = Rings)) + geom_jitter(alpha = 0.2)
+
+# Remove outlayers before plotting Height
+myData = abalone[abalone$Height<0.3,]
+ggplot(myData, aes(x = Height, y = Rings)) + geom_jitter(alpha = 0.2)
+
+
+myData = melt.data.frame(abalone[abalone$Height<0.3,], id.vars=c("Sex", "Rings"))
+myData[myData$variable=="Height",]$value = jitter(myData[myData$variable=="Height",]$value, factor = 3)
+ggplot(myData, aes(x = value, y = Rings)) + geom_jitter(alpha = 0.03) + facet_wrap( ~ variable, ncol = 2, scales = "free")
--- src/analysis/tae.R
+++ src/analysis/tae.R
@@ -59,108 +59,24 @@ dim(tae)
 # Get the data types of every field
 lapply(tae, class)
 
+# Check if there are missing values
+colSums(is.na(tae))
+
+# Get the statistical measurements (numeric variables)
+min       (tae$Size)        # Minimum
+quantile  (tae$Size, 0.25)  # 1st Quartile
+median    (tae$Size)        # Median
+mean      (tae$Size)        # Mean
+quantile  (tae$Size, 0.75)  # 3rd Quartile
+max       (tae$Size)        # Max
+
+var       (tae$Size)        # Variane
+sd        (tae$Size)        # Standard deviation
+
 # Get summarized information
 str(tae)      # Structure
-summary(tae)  # Statistical momenta
+summary(tae)  # Statistical measurements
 
 # By taking a look to the file, it is clear that 
 # the output variable is Class, thus we have to 
 # classify the data by this category
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ABALONE
-
-# Load the dataset and provide it with the 
-# structure from Keel
-abalone = read.csv(
-  "datasets/abalone/abalone.dat", 
-  comment.char="@"
-)
-names(abalone) = c(
-  "Sex", 
-  "Length", 
-  "Diameter", 
-  "Height", 
-  "Whole_weight", 
-  "Shucked_weight",
-  "Viscera_weight",
-  "Shell_weight",
-  "Rings"
-)
-
-
-abalone$Sex = factor(
-  abalone$Sex, 
-  levels = c(1, 2, 3), 
-  labels = c("M", "F", "I")
-  )
-
-
-# Get the data type and dimension
-class(abalone)
-dim(abalone)
-
-# Get summarized information
-str(abalone)      # Structure
-summary(abalone)  # Statistical momenta
-
-# By taking a look to the file, it is clear that 
-# the output variable is Class, thus we have to 
-# classify the data by this category
-
-ggplot(abalone, aes(x = Sex)) + geom_bar(stat = "count")
-
-meltData = melt.data.frame(data = abalone[,2:8])
-ggplot(meltData, aes(factor(variable), value)) + geom_boxplot() + coord_flip()
-
-ggplot(abalone, aes(Sex, Rings)) + geom_boxplot()
-
-ggplot(abalone, aes(Rings)) + geom_histogram(bins = 29)
-
-ggplot(abalone, aes(Shell_weight)) + geom_histogram(bins = 29)
-
-ggplot(abalone, aes(Shell_weight,   colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Viscera_weight, colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Shucked_weight, colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Whole_weight,   colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Height,         colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Diameter,       colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Length,         colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Rings,          colour = Sex)) + geom_freqpoly(bins = 29)
-# Histograms show that Male-Female distinction is not relevant, and does not characterize any of the above variables.
-# Nonetheless, Infants' dimensions are usually lower, as it is their age.
-# Statistical test is needed to confirm this statement.
-
-
-ggplot(abalone, aes(x = Shell_weight,   y = Rings)) + geom_jitter(alpha = 0.2)
-ggplot(abalone, aes(x = Viscera_weight, y = Rings)) + geom_jitter(alpha = 0.2)
-ggplot(abalone, aes(x = Shucked_weight, y = Rings)) + geom_jitter(alpha = 0.2)
-ggplot(abalone, aes(x = Whole_weight,   y = Rings)) + geom_jitter(alpha = 0.2)
-ggplot(abalone, aes(x = Diameter,       y = Rings)) + geom_jitter(alpha = 0.2)
-ggplot(abalone, aes(x = Length,         y = Rings)) + geom_jitter(alpha = 0.2)
-
-# Remove outlayers before plotting Height
-myData = abalone[abalone$Height<0.3,]
-ggplot(myData, aes(x = Height, y = Rings)) + geom_jitter(alpha = 0.2)
