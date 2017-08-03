ef2718dac07e11f5b1277a477679921e203cb077
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-08-03 21:20:04 +0200

    Remove plots

--- src/analysis/abalone.R
+++ src/analysis/abalone.R
@@ -71,22 +71,14 @@ colnames(a.s.m) = c(
 )
 rownames(a.s.m) = names(abalone2)
 
-a.s.m[,1] = 
-  sapply(abalone2, min)             # Minimum
-a.s.m[,2] =
-  sapply(abalone2, quantile, 0.25)  # 1st Quart.
-a.s.m[,3] =
-  sapply(abalone2, median)          # Median
-a.s.m[,4] =
-  sapply(abalone2, mean)            # Mean
-a.s.m[,5] =
-  sapply(abalone2, quantile, 0.75)  # 3rd Quart.
-a.s.m[,6] =
-  sapply(abalone2, max)             # Max
-a.s.m[,7] =
-  sapply(abalone2, var)             # Variance
-a.s.m[,8] =
-  sapply(abalone2, sd)              # Std. dev.
+a.s.m[,1] = sapply(abalone2, min)             # Minimum
+a.s.m[,2] = sapply(abalone2, quantile, 0.25)  # 1st Quart.
+a.s.m[,3] = sapply(abalone2, median)          # Median
+a.s.m[,4] = sapply(abalone2, mean)            # Mean
+a.s.m[,5] = sapply(abalone2, quantile, 0.75)  # 3rd Quart.
+a.s.m[,6] = sapply(abalone2, max)             # Max
+a.s.m[,7] = sapply(abalone2, var)             # Variance
+a.s.m[,8] = sapply(abalone2, sd)              # Std. dev.
 
 # Get the statistical measurements 
 # (categorical variables)
@@ -105,55 +97,45 @@ cor(abalone2)
 # the output variable is Class, thus we have to 
 # classify the data by this category
 
-ggplot(abalone, aes(x = Sex, fill = Sex)) + geom_bar(stat = "count")
+# PLOTS
 
-meltData1 = melt.data.frame(data = abalone[,2:4])
-meltData1$Measure = "length (mm)"
-ggplot(meltData1, aes(factor(variable), value)) + xlab("") + ylab("length (mm)") + geom_boxplot() + coord_flip()
+# Categorical variables (barplots)
+ggplot(abalone, aes(x = Sex, fill = Sex)) + 
+  geom_bar(stat = "count")
 
-meltData2 = melt.data.frame(data = abalone[,5:8])
-meltData2$Measure = "weight (grams)"
-ggplot(meltData2, aes(factor(variable), value)) + xlab("") + ylab("weight (grams)") + geom_boxplot() + coord_flip()
-
+# Numeric variables Boxplots
 meltData = rbind(meltData1, meltData2)
-ggplot(meltData, aes(factor(variable), value)) + xlab("") + ylab("") + geom_boxplot() + coord_flip() + facet_wrap( ~ Measure, ncol = 1, scales = "free")
+ggplot(meltData, aes(factor(variable), value)) + 
+  xlab("") + ylab("") + geom_boxplot() + coord_flip() + 
+  facet_wrap( ~ Measure, ncol=1, scales="free")
 
-
+# Distribution of rings
+ggplot(abalone, aes(Rings)) + geom_histogram(bins = 29)
 ggplot(abalone, aes(Sex, Rings)) + geom_boxplot()
 
-ggplot(abalone, aes(Rings)) + geom_histogram(bins = 29)
-
-ggplot(abalone, aes(Shell_weight)) + geom_histogram(bins = 29)
-
-ggplot(abalone, aes(Shell_weight,   colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Viscera_weight, colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Shucked_weight, colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Whole_weight,   colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Height,         colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Diameter,       colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Length,         colour = Sex)) + geom_freqpoly(bins = 29)
-ggplot(abalone, aes(Rings,          colour = Sex)) + geom_freqpoly(bins = 29)
+# Distribution of numeric variables against sex
+# Remove outlayers
+abalone.reshaped = melt(abalone[abalone$Height<0.3,]) 
+ggplot(abalone.reshaped, aes(value, colour = Sex)) + 
+  geom_freqpoly(bins = 29) + 
+  facet_wrap( ~ variable, ncol = 2, scales="free")
 
 
-abalone.reshaped = melt(abalone[abalone$Height<0.3,]) # Remove outlayers
-ggplot(abalone.reshaped, aes(value, colour = Sex)) + geom_freqpoly(bins = 29) + facet_wrap( ~ variable, ncol = 2, scales = "free")
-# Histograms show that Male-Female distinction is not relevant, and does not characterize any of the above variables.
-# Nonetheless, Infants' dimensions are usually lower, as it is their age.
+# Histograms show that Male-Female distinction is not relevant, and 
+# does not characterize any of the above variables.
+# Nonetheless, Infants' dimensions are usually lower, 
+# as is their age.
 # Statistical test is needed to confirm this statement.
 
 
-ggplot(abalone, aes(x = Shell_weight,   y = Rings)) + geom_jitter(alpha = 0.2)
-ggplot(abalone, aes(x = Viscera_weight, y = Rings)) + geom_jitter(alpha = 0.2)
-ggplot(abalone, aes(x = Shucked_weight, y = Rings)) + geom_jitter(alpha = 0.2)
-ggplot(abalone, aes(x = Whole_weight,   y = Rings)) + geom_jitter(alpha = 0.2)
-ggplot(abalone, aes(x = Diameter,       y = Rings)) + geom_jitter(alpha = 0.2)
-ggplot(abalone, aes(x = Length,         y = Rings)) + geom_jitter(alpha = 0.2)
-
-# Remove outlayers before plotting Height
-myData = abalone[abalone$Height<0.3,]
-ggplot(myData, aes(x = Height, y = Rings)) + geom_jitter(alpha = 0.2)
-
-
-myData = melt.data.frame(abalone[abalone$Height<0.3,], id.vars=c("Sex", "Rings"))
-myData[myData$variable=="Height",]$value = jitter(myData[myData$variable=="Height",]$value, factor = 3)
+# Scatterplots
+myData = melt.data.frame(
+  abalone[abalone$Height<0.3,], 
+  id.vars=c("Sex", "Rings")
+)
+myData[myData$variable=="Height",] = 
+  within(myData[myData$variable=="Height",], {
+    value = jitter(value, factor = 3)
+  }
+)
 ggplot(myData, aes(x = value, y = Rings)) + geom_jitter(alpha = 0.03) + facet_wrap( ~ variable, ncol = 2, scales = "free")
