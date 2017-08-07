04d56da4007b542e0d1b128db270bdc44854f6a8
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-08-07 17:38:50 +0200

    Enhance plots and ks.test Rings

--- src/analysis/abalone.R
+++ src/analysis/abalone.R
@@ -14,18 +14,18 @@ library(reshape)
 
 # ABALONE
 
-# Load the dataset and provide it with the 
+# Load the dataset and provide it with the
 # structure from Keel
 abalone = read.csv(
-  "datasets/abalone/abalone.dat", 
+  "datasets/abalone/abalone.dat",
   comment.char="@"
 )
 names(abalone) = c(
-  "Sex", 
-  "Length", 
-  "Diameter", 
-  "Height", 
-  "Whole_weight", 
+  "Sex",
+  "Length",
+  "Diameter",
+  "Height",
+  "Whole_weight",
   "Shucked_weight",
   "Viscera_weight",
   "Shell_weight",
@@ -34,8 +34,8 @@ names(abalone) = c(
 
 
 abalone$Sex = factor(
-  abalone$Sex, 
-  levels = c(1, 2, 3), 
+  abalone$Sex,
+  levels = c(1, 2, 3),
   labels = c("M", "F", "I")
 )
 
@@ -50,7 +50,7 @@ lapply(abalone, class)
 # Check if there are missing values
 colSums(is.na(abalone))
 
-# Get the statistical measurements 
+# Get the statistical measurements
 # (numeric variables) and save them into a.s.m
 # (abalone.statistical.measurements matrix)
 abalone2 = subset(abalone, select = -Sex)
@@ -60,13 +60,13 @@ a.s.m = matrix(
   ncol = 8
 )
 colnames(a.s.m) = c(
-  "Min.", 
-  "1st Qu.", 
-  "Median", 
-  "Mean", 
-  "3rd Qu.", 
-  "Max.", 
-  "Var.", 
+  "Min.",
+  "1st Qu.",
+  "Median",
+  "Mean",
+  "3rd Qu.",
+  "Max.",
+  "Var.",
   "SD"
 )
 rownames(a.s.m) = names(abalone2)
@@ -80,7 +80,7 @@ a.s.m[,6] = sapply(abalone2, max)             # Max
 a.s.m[,7] = sapply(abalone2, var)             # Variance
 a.s.m[,8] = sapply(abalone2, sd)              # Std. dev.
 
-# Get the statistical measurements 
+# Get the statistical measurements
 # (categorical variables)
 a.s.m2 = as.data.frame( table(abalone$Sex) )
 names(a.s.m2)[1] = "Sex"
@@ -91,51 +91,73 @@ str(abalone)      # Structure
 summary(abalone)  # Statistical measurements
 
 # Get correlation matrix
-cor(abalone2)
+cor.matrix = cor(abalone2)
 
-# By taking a look to the file, it is clear that 
-# the output variable is Class, thus we have to 
+# By taking a look to the file, it is clear that
+# the output variable is Class, thus we have to
 # classify the data by this category
 
 # PLOTS
 
 # Categorical variables (barplots)
-ggplot(abalone, aes(x = Sex, fill = Sex)) + 
+ggplot(abalone, aes(x = Sex, fill = Sex)) +
   geom_bar(stat = "count")
 
 # Numeric variables Boxplots
+meltData1 = melt.data.frame(data = abalone[,2:4])
+meltData1$Measure = "length (mm)"
+
+meltData2 = melt.data.frame(data = abalone[,5:8])
+meltData2$Measure = "weight (grams)"
+
 meltData = rbind(meltData1, meltData2)
-ggplot(meltData, aes(factor(variable), value)) + 
-  xlab("") + ylab("") + geom_boxplot() + coord_flip() + 
+ggplot(meltData, aes(factor(variable), value)) +
+  xlab("") + ylab("") + geom_boxplot() + coord_flip() +
   facet_wrap( ~ Measure, ncol=1, scales="free")
 
 # Distribution of rings
-ggplot(abalone, aes(Rings)) + geom_histogram(bins = 29)
-ggplot(abalone, aes(Sex, Rings)) + geom_boxplot()
+ggplot(abalone, aes(Rings, fill = Sex, color = Sex)) + geom_freqpoly(bins = 29)
+ggplot(abalone, aes(Sex, Rings, fill = Sex)) + geom_boxplot()
+ggplot(abalone, aes(Rings, fill = Sex, color = Sex)) + stat_ecdf(bins = 29) + ylab("CDF")
+ggplot(abalone, aes(Rings, fill = Sex, color = Sex)) + stat_density(alpha = .8)
 
 # Distribution of numeric variables against sex
 # Remove outlayers
-abalone.reshaped = melt(abalone[abalone$Height<0.3,]) 
-ggplot(abalone.reshaped, aes(value, colour = Sex)) + 
-  geom_freqpoly(bins = 29) + 
+abalone.reshaped = melt(abalone[abalone$Height<0.3,])
+ggplot(abalone.reshaped, aes(value, colour = Sex)) +
+  geom_freqpoly(bins = 29) +
   facet_wrap( ~ variable, ncol = 2, scales="free")
 
-
-# Histograms show that Male-Female distinction is not relevant, and 
+# Histograms show that Male-Female distinction is not relevant, and
 # does not characterize any of the above variables.
-# Nonetheless, Infants' dimensions are usually lower, 
+# Nonetheless, Infants' dimensions are usually lower,
 # as is their age.
 # Statistical test is needed to confirm this statement.
 
 
 # Scatterplots
 myData = melt.data.frame(
-  abalone[abalone$Height<0.3,], 
+  abalone[abalone$Height<0.3,],
   id.vars=c("Sex", "Rings")
 )
-myData[myData$variable=="Height",] = 
+myData[myData$variable=="Height",] =
   within(myData[myData$variable=="Height",], {
     value = jitter(value, factor = 3)
   }
 )
 ggplot(myData, aes(x = value, y = Rings)) + geom_jitter(alpha = 0.03) + facet_wrap( ~ variable, ncol = 2, scales = "free")
+
+
+# Check whether the Rings distributions are the same
+mean(abalone[abalone$Sex=="F",]$Rings)
+summary(abalone[abalone$Sex=="F",]$Rings)
+mean(abalone[abalone$Sex=="M",]$Rings)
+summary(abalone[abalone$Sex=="M",]$Rings)
+
+ks.test(abalone[abalone$Sex=="M",]$Rings,abalone[abalone$Sex=="F",]$Rings)
+# In the studied sample, there are more old females than old males. Actually, the distributions can be proved to not be the same.
+# This can indicate that females usually live longer than males.
+# This fact must be taken into account to test if there is any secondary sex charasteristic,
+# i.e. it does not suffice to test wheter length distribution in females is the same than length distribution in males,
+# it is necessary to test this taking age into account.
+# From the minimum age of both female and male, it can be suspected that primary sex characteristics are easier detected on males.
