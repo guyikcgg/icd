d0180ebfbb3eb387be05f444a5459c79801029c1
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-08-14 20:35:01 +0200

    Enhance plot

--- src/regression/knn-regression.R
+++ src/regression/knn-regression.R
@@ -8,6 +8,8 @@
 # Build the workspace
 source("../regression/build-workspace.R")
 
+abalone = abalone[abalone$Height<0.3,]
+
 # Get simple knn models
 simple.knn.fit = list()
 for (i in 1:(length(abalone)-1)) {
@@ -21,22 +23,18 @@ myData1 = melt.data.frame(
   abalone,
   id.vars=c("Sex", "Rings")
 )
-myData1[myData$variable=="Height",] =
-  within(myData[myData$variable=="Height",], {
-    value = jitter(value, factor = 3)
-  })
 
 myData2 = data.frame()
 for (variable in names(simple.knn.fit)[2:8]) {
   Prediction = simple.knn.fit[[variable]]$fitted.values
-  myData2 = rbind(myData2, data.frame(variable, Prediction))
+  myData2 = rbind(myData2, data.frame(Prediction))
 }
 
 myData = cbind(myData1, myData2)
 
 Data = "Original data"
 b = "Prediction"
-colorPalette <- c("#000000", "#0072B2")
+colorPalette <- c("#000000", "#56B4E9")
 
 ggplot(myData) + 
   geom_point(aes(x = value, y = Rings, color = Data), alpha = 0.03) + 
@@ -45,4 +43,5 @@ ggplot(myData) +
   xlab("") +
   scale_colour_manual(values=colorPalette) +
   guides(colour = guide_legend(override.aes = list(alpha = 1)))
+
 
