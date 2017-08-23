7b350a69c213d4587bcbcc1028fc8d934c61a065
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-08-23 18:35:15 +0200

    New plots and statistical tests

--- src/classification/classification.R
+++ src/classification/classification.R
@@ -16,13 +16,6 @@ tae$Size = normalize(tae$Size)
 tae.tra.dt = lapply(tae.tra.dt, normalize.tae)
 tae.tst.dt = lapply(tae.tst.dt, normalize.tae)
 
-# Some plots
-#plot(tae, col = tae$Class)
-tae2 = tae
-tae2$Instructor = as.integer(tae$Instructor)
-tae2$Course = as.integer(tae$Course)
-#ggpairs(tae2, aes(color = Class, alpha = 0.3))
-
 
 
 ################################
@@ -75,19 +68,26 @@ myData[myData$variable=="Acc_mean",]$lineSize = 2
 max(Acc$Acc_mean)
 which.max(Acc$Acc_mean)
 
-ggplot(myData, aes(x = k, y = value, color = variable, size = lineSize)) + 
-  geom_line() + scale_size(range = c(0.5, 2), guide="none") + ylab("Accuracy")
+ggplot(
+  myData, 
+  aes(x = k, y = value, color = variable, size = lineSize)
+) + 
+  geom_line() + scale_size(range = c(0.5, 2), guide="none") + 
+  ylab("Accuracy")
 ## It seems that k=1 performs the best, with Acc = 0.61
-# This plot could also be done with training data, to test for overfitting
+# This plot could also be done with training data 
+# in order to test for overfitting
 
 
 # Fill in the table in the document
-Acc = matrix(ncol = 10, nrow = 10)
-rownames(Acc) = paste("K", 1:10, sep = "")
-colnames(Acc) = c(paste("Acc", 1:9, sep = ""), "Acc_mean")
-Kappa = matrix(ncol = 10, nrow = 10)
-rownames(Kappa) = paste("K", 1:10, sep = "")
-colnames(Kappa) = c(paste("Kappa", 1:9, sep = ""), "Kappa_mean")
+Acc.knn = matrix(ncol = 10, nrow = 10)
+rownames(Acc.knn) = paste("K", 1:10, sep = "")
+colnames(Acc.knn) = 
+  c(paste("Acc", 1:9, sep = ""), "Acc_mean")
+Kappa.knn = matrix(ncol = 10, nrow = 10)
+rownames(Kappa.knn) = paste("K", 1:10, sep = "")
+colnames(Kappa.knn) = 
+  c(paste("Kappa", 1:9, sep = ""), "Kappa_mean")
 for (m in 1:9) {
     myAcc   = rep(0, 10)
     myKappa = rep(0, 10)
@@ -104,11 +104,11 @@ for (m in 1:9) {
       myAcc[i]   = cm$overall[1]
       myKappa[i] = cm$overall[2]
     }
-    Acc[,m]   = myAcc
-    Kappa[,m] = myKappa
+    Acc.knn[,m]   = myAcc
+    Kappa.knn[,m] = myKappa
 }
-Acc[,10] = rowMeans(Acc[,1:9])
-Kappa[,10] = rowMeans(Kappa[,1:9])
+Acc.knn[,10]   = rowMeans(Acc.knn[,1:9])
+Kappa.knn[,10] = rowMeans(Kappa.knn[,1:9])
 
 
 ######################################
@@ -120,7 +120,13 @@ sapply(tae.original[1:5], shapiro.test)
 
 myData = melt.data.frame(
   data = tae.original,
-  measure.vars = c("Native", "Instructor", "Course", "Semester", "Size")
+  measure.vars = c(
+    "Native",
+    "Instructor",
+    "Course",
+    "Semester",
+    "Size"
+  )
 )
 ggplot(myData) + 
   stat_qq(aes(sample = value)) + 
@@ -164,13 +170,54 @@ mean(Acc.LDA$Acc)
 # meaning a loss of less than 5% in accuracy.
 
 
-library(ISLR)
-library(klaR)
-plot(lda.fit)
+# Fill in the table in the document
+Acc.lda = matrix(ncol = 10, nrow = 10)
+rownames(Acc.lda) = paste("K", 1:10, sep = "")
+colnames(Acc.lda) = 
+  c(paste("Acc", 1:9, sep = ""), "Acc_mean")
+Kappa.lda = matrix(ncol = 10, nrow = 10)
+rownames(Kappa.lda) = paste("K", 1:10, sep = "")
+colnames(Kappa.lda) = 
+  c(paste("Kappa", 1:9, sep = ""), "Kappa_mean")
+for (m in 1:9) {
+  myAcc   = rep(0, 10)
+  myKappa = rep(0, 10)
+  for (i in 1:10) {
+    lda.fit = lda(
+      Class ~ .,
+      data = tae.tra[[i]]
+    )
+    lda.pred = predict(lda.fit, tae.tst.dt[[i]])
+    
+    cm = confusionMatrix(
+      table(lda.pred$class, tae.tst.cl[[i]])
+    )
+    myAcc[i]   = cm$overall[1]
+    myKappa[i] = cm$overall[2]
+  }
+  Acc.lda[,m]   = myAcc
+  Kappa.lda[,m] = myKappa
+}
+Acc.lda[,10] = rowMeans(Acc.lda[,1:9])
+Kappa.lda[,10] = rowMeans(Kappa.lda[,1:9])
 
-partimat(Class~Size+Instructor, data = tae.tra[[1]], method = "lda")
-partimat(Class~Size+Course, data = tae.tra[[1]], method = "lda")
-partimat(Class~Instructor+Course, data = tae.tra[[1]], method = "lda")
+
+## Some plots
+partimat(
+  Class~Size+Instructor, 
+  data = tae.tra[[1]],
+  method = "lda"
+)
+partimat(
+  Class~Size+Course,
+  data = tae.tra[[1]],
+  method = "lda"
+)
+partimat(
+  Class~Instructor+Course,
+  data = tae.tra[[1]],
+  method = "lda"
+)
 
 
 
@@ -206,16 +253,103 @@ mean(Acc.QDA$Acc)
 # QDA performs poorly if every variable is considered.
 
 
-library(ISLR)
-library(klaR)
-plot(qda.fit)
+# Fill in the table in the document
+Acc.qda = matrix(ncol = 10, nrow = 10)
+rownames(Acc.qda) = paste("K", 1:10, sep = "")
+colnames(Acc.qda) = 
+  c(paste("Acc", 1:9, sep = ""), "Acc_mean")
+Kappa.qda = matrix(ncol = 10, nrow = 10)
+rownames(Kappa.qda) = paste("K", 1:10, sep = "")
+colnames(Kappa.qda) = 
+  c(paste("Kappa", 1:9, sep = ""), "Kappa_mean")
+for (m in 1:9) {
+  myAcc   = rep(0, 10)
+  myKappa = rep(0, 10)
+  for (i in 1:10) {
+    qda.fit = qda(
+      Class ~ .,
+      data = tae.tra[[i]]
+    )
+    qda.pred = predict(qda.fit, tae.tst.dt[[i]])
+    
+    cm = confusionMatrix(
+      table(qda.pred$class, tae.tst.cl[[i]])
+    )
+    myAcc[i]   = cm$overall[1]
+    myKappa[i] = cm$overall[2]
+  }
+  Acc.qda[,m]   = myAcc
+  Kappa.qda[,m] = myKappa
+}
+Acc.qda[,10] = rowMeans(Acc.qda[,1:9])
+Kappa.qda[,10] = rowMeans(Kappa.qda[,1:9])
 
-partimat(Class~Size+Instructor, data = tae.tra[[1]], method = "qda")
-partimat(Class~Size+Course, data = tae.tra[[1]], method = "qda")
-partimat(Class~Instructor+Course, data = tae.tra[[1]], method = "qda")
+
+## Some plots
+partimat(
+  Class~Size+Instructor, 
+  data = tae.tra[[1]], 
+  method = "qda"
+)
+partimat(
+  Class~Size+Course, 
+  data = tae.tra[[1]], 
+  method = "qda"
+)
+partimat(
+  Class~Instructor+Course, 
+  data = tae.tra[[1]], 
+  method = "qda"
+)
 
 
 
-# Algorithm comparison
+########################
+# Algorithm comparison #
+########################
 
+# Acc and Kappa for every k-fold
+Acc = cbind(Acc.knn[,10], Acc.lda[,10], Acc.qda[,10])
+Acc = rbind(Acc, mean = colMeans(Acc))
+colnames(Acc) = c("kNN", "LDA", "QDA")
+Kappa = cbind(Kappa.knn[,10], Kappa.lda[,10], Kappa.qda[,10])
+Kappa = rbind(Kappa, mean = colMeans(Kappa))
+colnames(Kappa) = c("kNN", "LDA", "QDA")
 
+# Check whether there is a method outperforming the others
+friedman.test(as.matrix(Acc))
+
+## Low p-value clearly shows that there's a method 
+## outperforming the others
+
+groups = rep(1:ncol(Acc), each = nrow(Acc))
+pairwise.wilcox.test(
+  Acc, 
+  groups, 
+  p.adjust.method = "holm", 
+  paired = TRUE
+)
+
+# There are significant differences amongst the three methods,
+# a plot will clarify it
+
+myAcc = as.data.frame(cbind(Acc, k.fold = 1:11))[1:10,]
+myData = melt.data.frame(
+  data = myAcc,
+  id.vars = c("k.fold"),
+  variable_name = "Method"
+)
+names(myData)[3] = "Acc"
+ggplot(myData, aes(x = k.fold, y = Acc, fill = Method)) + 
+  geom_col(position = "dodge")
+
+myData = data.frame(Measure = c("Acc.mean", "Kappa.mean"))
+myData = cbind(myData, rbind(t(Acc[11,]), t(Kappa[11,])))
+myData = melt.data.frame(
+  data = as.data.frame(myData),
+  id.vars = "Measure",
+  variable_name = "Method"
+)
+ggplot(myData, aes(x = Method, y = value, fill = Method)) + 
+  geom_col(position = "dodge") + ylab("") +
+  facet_wrap(~Measure, nrow = 1)
